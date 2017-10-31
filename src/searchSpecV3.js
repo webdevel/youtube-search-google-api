d63c1844a676986dc8bcc5ef69e2ac7e156393ac
Steven Garcia <webwhammy@gmail.com>
2017-10-31 16:44:34 -0700

    move search specification from json to js.

--- src/searchSpecV3.js
+++ src/searchSpecV3.js
@@ -1,4 +1,4 @@
-{
+export const searchSpec = {
   "brief": "A search result contains information about a YouTube video, channel, or playlist that matches the search parameters specified in an API request. While a search result points to a uniquely identifiable resource, like a video, it does not have its own persistent data.",
   "type": "YouTubeSearchRequest",
   "method": "GET",
